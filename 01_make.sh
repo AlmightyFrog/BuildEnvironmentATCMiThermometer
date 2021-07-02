5bb7a935f4000687d6eeb5d1daddcc546a2f63a4
Oskar Joelsson <oskar.joelsson@forefront.se>
2021-07-02 12:36:53 +0200

    in 01_make select devicetype

--- 01_make.sh
+++ 01_make.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+DEVICE_TYPE="LYWSD03MMC" # LYWSD03MMC, MHO_C401 or CGG1
+
 echo \#\#
 echo \#\# Build
 echo \#\#
@@ -7,6 +10,7 @@ if [ -d "ATC_MiThermometer/ATC_Thermometer" ]; then
     echo \#\#
     echo \#\# Built atc1441 ATC_MiThermometer
 else
+    sed -i '' -E "s/#define DEVICE_TYPE.*\/\//#define DEVICE_TYPE\t\t\tDEVICE_$DEVICE_TYPE \/\//g" ATC_MiThermometer/src/app_config.h
     docker run --rm -v $PWD:/app -it tc32 -c "cd ATC_MiThermometer && TEL_PATH=../SDK make $@"
     echo \#\#
     echo \#\# Built pvvx ATC_MiThermometer \(if you\'re not using windows an error will occor on validation with tl_check_fw2.exe, it\'s okey\)
@@ -15,5 +19,6 @@ echo \#\#
 
 mkdir -p firmware
 echo \#\#
-cp ATC_MiThermometer/ATC_Thermometer.bin firmware/ATC_Thermometer_"$(date +%s)".bin && echo \#\# Here is your awesome firmware: firmware/ATC_Thermometer_"$(date +%s)".bin || echo \#\# No file found
+firmware_out=firmware/ATC_Thermometer_"$DEVICE_TYPE"_"$(date +%s)".bin
+cp ATC_MiThermometer/ATC_Thermometer.bin $firmware_out && echo \#\# Here is your awesome firmware: $firmware_out || echo \#\# No file found
 echo  \#\#
